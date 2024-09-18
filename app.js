43da252d2252fc6a7934c720a47c592ecbb58b7f
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-18 16:01:39 +0300

    feat: coloring our inputs

--- app.js
+++ app.js
@@ -20,13 +20,20 @@ function generaeHex() {
 function randomColors() {
   colorDivs.forEach((div, index) => {
     const hexText = div.children[0];
-    console.log("🚀 ~ colorDivs.forEach ~ hexText:", hexText);
     const randomColor = generaeHex();
 
     //add the bsckground Color
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
     checkTextContract(randomColor, hexText);
+    // initailze colorize slider
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -37,6 +44,24 @@ function checkTextContract(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //scale saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+  //brightness scale
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+  //input update
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)},${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)},${scaleBright(
+    0.5
+  )},
+  ${scaleBright(1)})`;
+  //hue is speacial :
+  hue.style.backgroundImage = `linear-gradient(to right,rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 randomColors();
