f9647a728ae48bcc246894e95067a15eefcf16a8
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-18 12:52:23 +0300

    feat: generate Colors

---
+++ app.js
@@ -0,0 +1,32 @@
+// Selectors
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll("input[type='range']");
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+//Functions
+//color Generator
+function generaeHex() {
+  //   const letters = "0123456789ABCDEF";
+  //   let hash = "#";
+  //   for (let i = 0; i < 6; i++) {
+  //     hash += letters[Math.floor(Math.random() * 16)];
+  //   }
+  //   return hash;
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    console.log("🚀 ~ colorDivs.forEach ~ hexText:", hexText);
+    const randomColor = generaeHex();
+
+    //add the bsckground Color
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor.toString();
+  });
+}
+
+randomColors();
