64915067972603d4235a2e43ae967993e409fbd4
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-18 18:52:13 +0300

    feat:Updating the Bg-color

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll("input[type='range']");
 const currentHexes = document.querySelectorAll(".color h2");
 let initialColors;
+//add event Listeners
+sliders.forEach((slider) => {
+  slider.addEventListener("input", hslControls);
+});
+
 //Functions
 //color Generator
 function generaeHex() {
@@ -62,6 +67,27 @@ function colorizeSliders(color, hue, brightness, saturation) {
   ${scaleBright(1)})`;
   //hue is speacial :
   hue.style.backgroundImage = `linear-gradient(to right,rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+
+function hslControls(e) {
+  const index =
+    e.target.getAttribute("data-bright") ||
+    e.target.getAttribute("data-sat") ||
+    e.target.getAttribute("data-hue");
+
+  let sliders = e.target.parentElement.querySelectorAll('input[type="range"');
+  const hue = sliders[0];
+  const bright = sliders[1];
+  const sat = sliders[2];
+
+  let bgColor = colorDivs[index].querySelector("h2").innerText;
+
+  let color = chroma(bgColor)
+    .set("hsl.s", sat.value)
+    .set("hsl.l", bright.value)
+    .set("hsl.h", hue.value);
+
+  colorDivs[index].style.backgroundColor = color;
 }
 
 randomColors();
