317d16914bd871c574a0d286e5a385e6eb37ee81
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-19 13:30:35 +0300

    feat: updating Slides Values

--- app.js
+++ app.js
@@ -46,6 +46,8 @@ function randomColors() {
 
     colorizeSliders(color, hue, brightness, saturation);
   });
+  //reset inputs
+  resetInputs();
 }
 
 function checkTextContract(color, text) {
@@ -107,6 +109,26 @@ function updateTextUI(index) {
   for (icon of icons) {
     checkTextContract(color, icon);
   }
+}
+function resetInputs() {
+  const sliders = document.querySelectorAll(".sliders input");
+  sliders.forEach((slider) => {
+    if (slider.name === "hue") {
+      const hueColor = initialColors[slider.getAttribute("data-hue")];
+      const hueValue = chroma(hueColor).hsl()[0];
+      slider.value = Math.floor(hueValue);
+    }
+    if (slider.name === "Brightness") {
+      const brightColor = initialColors[slider.getAttribute("data-bright")];
+      const brightValue = chroma(brightColor).hsl()[2];
+      slider.value = Math.floor(brightValue * 100) / 100;
+    }
+    if (slider.name === "Saturation") {
+      const satColor = initialColors[slider.getAttribute("data-sat")];
+      const satValue = chroma(satColor).hsl()[1];
+      slider.value = Math.floor(satValue * 100) / 100;
+    }
+  });
 }
 
 randomColors();
